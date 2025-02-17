a44b8a1b1995f9bc136ee30a202d8fdf1fa629a6
Majosick <majoss97@gmail.com>
2025-02-17 14:59:30 +0100

    update of calculator & employees

--- src/main/java/Calculator/Calculator.java
+++ src/main/java/Calculator/Calculator.java
@@ -2,21 +2,21 @@ package Calculator;
 
 public class Calculator {
 
-    public int divide(int number1, int number2){
-        try{
-            return number1/number2;
-        }
-        catch (Exception e){
+    public int divide(int number1, int number2) {
+        try {
+            return number1 / number2;
+        } catch (Exception e) {
             e.printStackTrace();
             return 0;
-        }
-        finally {
+        } finally {
             System.out.println("ja sie i tak wykonam");
         }
     }
 
     public static void main(String[] args) {
         Calculator c = new Calculator();
-        c.divide(1,0);
+        int divided = c.divide(1, 0);
+//        int divided = c.divide(7, 2);
+//        System.out.println(divided);
     }
 }
--- src/main/java/Employee/Employee.java
+++ src/main/java/Employee/Employee.java
@@ -1,5 +1,8 @@
 package Employee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
 
     private String firstName;
@@ -22,5 +25,26 @@ public class Employee {
 
     public int getSalary() {
         return salary;
+    }
+
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Piotr", 10000, "szef");
+        Employee e2 = new Employee("Adam", 15000, "szef");
+        Employee e3 = new Employee("Mati", 20000, "szef");
+
+        e2.increaseSalaryBy(2000);
+
+        List<Employee> lista = new ArrayList<>();
+        lista.add(e1);
+        lista.add(e2);
+        lista.add(e3);
+
+        for (Employee e : lista) {
+            if (e.getSalary() < 20000) {
+                System.out.println(e.getFirstName() + " zarabia mniej niż 20000");
+            }
+        }
+        lista.stream();
     }
 }
