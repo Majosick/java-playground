4ee51267a365a034f643dcf6cd711388fc599177
Majosick <majoss97@gmail.com>
2025-02-17 14:46:38 +0100

    update anagram and untrack target folder

--- src/main/java/Anagram/Anagram.java
+++ src/main/java/Anagram/Anagram.java
@@ -6,18 +6,24 @@ import java.util.Scanner;
 public class Anagram {
 
     public static void main(String[] args) {
-
-        System.out.println("Hi, eneter two words, and I'll tell yuo whether they are anagrams");
         Scanner scanner = new Scanner(System.in);
-        String firstWord = scanner.nextLine();
-        String secondWord = scanner.nextLine();
-        String[] table1 = firstWord.split("");
-        String[] table2 = secondWord.split("");
-        Arrays.sort(table1);
-        Arrays.sort(table2);
-        if(Arrays.equals(table1, table2)) {
-            System.out.println("Dobrze");
-        } else System.out.println("Zle");
-
+        boolean continueLoop = true;
+        while (continueLoop) {
+            System.out.println("Enter two words, and I'll tell you whether they are anagrams");
+            String firstWord = scanner.nextLine();
+            String secondWord = scanner.nextLine();
+            String[] table1 = firstWord.split("");
+            String[] table2 = secondWord.split("");
+            Arrays.sort(table1);
+            Arrays.sort(table2);
+            if (Arrays.equals(table1, table2)) {
+                System.out.println("They are anagrams");
+            } else {
+                System.out.println("They are not anagrams");
+            }
+            System.out.println("Do you want to enter new pair? (Y/N)");
+            String input = scanner.nextLine();
+            continueLoop = input.equalsIgnoreCase("Y"); // x will be true if input is "N" (case-insensitive)
+        }
     }
 }
